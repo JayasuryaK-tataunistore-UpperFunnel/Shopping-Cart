4493a867f9ecdca550cf063c53fe3930f07370c6
JayasuryaK-tataunistore-UpperFunnel <k.jayasurya321@gmail.com>
2024-11-24 16:54:03 +0530

    Project initialisation

---
+++ src/main/java/com/surya/easyshop/model/Category.java
@@ -0,0 +1,25 @@
+package com.surya.easyshop.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id ;
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    List<Product> products;
+}
